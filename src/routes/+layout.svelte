5c364ac9e291447e4dc63857fea29f5777a65d39
goldentoaste <rayg2375@gmail.com>
2023-02-02 18:52:20 -0800

    setups

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+
+
+<nav>
+
+    <a href="/">Home</a>
+    <a href="projects">Projects</a>
+    <a href="others">Others</a>
+</nav>
+
+<slot/>
+
+<style>
+
+
+    nav {
+        background-color: #dddddd;
+    }
+</style>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,2 @@
+
+<!-- Home page as a conponent -->
