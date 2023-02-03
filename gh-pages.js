1355d4059b5b2df69c13730f44432e15db46af5c
goldentoaste <rayg2375@gmail.com>
2023-02-02 22:46:50 -0800

    gh pages configs

---
+++ gh-pages.js
@@ -0,0 +1,18 @@
+
+import { publish } from 'gh-pages';
+
+publish(
+ 'build', // path to public directory
+ {
+  branch: 'gh-pages',
+  repo: 'https://github.com/goldentoaste/portfolio.git', // Update to point to your repository
+  user: {
+   name: 'goldentoaste', // update to use your name
+   email: 'rayg2375@gmail.com' // Update to use your email
+  },
+  dotfiles: true
+  },
+  () => {
+   console.log('Deploy Complete!');
+  }
+);
