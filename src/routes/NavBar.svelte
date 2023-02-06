141710b0337a3b52e5ee21553b462ec701d07169
Goldentoaste <rayg2375@gmail.com>
2023-02-05 17:54:27 -0800

    styling wip

--- src/routes/NavBar.svelte
+++ src/routes/NavBar.svelte
@@ -1,18 +1,41 @@
-
 <script lang="ts">
     type NavItem = {
         name: string;
         dest: string;
-    }
+    };
 
     export let items: NavItem[] = [];
-
 </script>
 
-
 <nav>
-
     {#each items as item}
-        <a href="{item.dest}">{item.name}</a>
+        <a class="navitem" href={item.dest}>{item.name}</a>
     {/each}
+    <button
+        on:click={() => {
+            window.document.body.classList.toggle('lightmode');
+        }}
+    >
+        toggle darkmode</button
+    >
 </nav>
+
+<style>
+
+    nav{
+        display: flex;
+        width: 100%;
+        position: fixed;
+        top: 0;
+        left:0;
+        background-color: var(--fg);
+        
+    }
+
+    .navitem {
+        display: block;
+
+    }
+
+
+</style>
