8ccdada1cdfe74447d5e4e97fcc117fd7232fc25
goldentoaste <rayg2375@gmail.com>
2023-02-05 20:09:16 -0800

    front page update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,13 @@
     import NavBar from "./NavBar.svelte";
 
 </script>
-<h1>Ray Gong's website</h1>
+<h1>Ray Gong's website! (Under Construction)</h1>
+
+<div class="container">
+
+    <p>Resume <a href="https://github.com/goldentoaste/portfolio/raw/main/FileStorage/RayGongResumeFeb2-2023.pdf">here</a></p>
+    <p>Github <a href="https://github.com/goldentoaste">here</a></p>
+    <p>Email: rayg2375@gmail.com</p>
+</div>
 
 <Intro></Intro>
