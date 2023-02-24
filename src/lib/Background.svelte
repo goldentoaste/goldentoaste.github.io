addc4dee66508d8ea4e1e11fe0d6fb6e11e9ea69
Goldentoaste <rayg2375@gmail.com>
2023-02-23 20:33:45 -0800

    tbc

--- src/lib/Background.svelte
+++ src/lib/Background.svelte
@@ -13,7 +13,7 @@
     }}
 >
     {#each [...Array(4).keys()] as index}
-    <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMin meet" style="--baseX: {index * 50 + linePos}; --baseY: {index * 50 + linePos};transform: translate(calc(var(--baseX) + var(--offsetX))px calc(var(--baseY) + var(--offsetY))px);" width="100" height="100" viewBox="0 0 100 100">
+    <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMin meet" style="--baseX: {index * 50 + linePos}; --baseY: {index * 50 + linePos};transform: translate(calc(var(--baseX)px + var(--offsetX)px) calc(var(--baseY)px + var(--offsetY)px));" width="100" height="100" viewBox="0 0 100 100">
         <path d="M 0 0 L 100 100" stroke="var(--fg)" stroke-width="1"/>
     </svg>
     {/each}
