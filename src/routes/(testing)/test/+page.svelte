8395248ee8284180a41178dfbc3eb6c8a3fb6f65
goldentoaste <rayg2375@gmail.com>
2023-07-20 19:54:56 -0700

    Update +page.svelte

--- src/routes/(testing)/test/+page.svelte
+++ src/routes/(testing)/test/+page.svelte
@@ -1,49 +1,51 @@
 <script lang="ts">
-	let name = 'world';
+	import { onMount } from "svelte";
+
+	let name = "world";
 
 	let maxHeight = 100;
 	let minHeight = 50;
 	let offset = 100;
-	let scroll : number = 0;
-    let windowHeight = 100;
+	let scroll: number = 0;
+	let windowHeight = 100;
 
-    let stuff = 0;
-    $: console.log(stuff    )
-
+	onMount(() => {
+		windowHeight = document.body.clientHeight;
+	});
 </script>
 
-
-<svelte:window bind:scrollY={scroll} on:resize={()=>{
-    console.log("asdsadas")
-    windowHeight = document.body.clientHeight;
-}}/>
+<svelte:window
+	bind:scrollY={scroll}
+	on:resize={() => {
+		windowHeight = document.body.clientHeight;
+	}}
+/>
 
 <div id="wrapper" style="height:{windowHeight}px;">
-	<img style="height: {Math.max(minHeight, Math.min(maxHeight, maxHeight - scroll + offset) )}px;" alt=""  src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/498px-Svelte_Logo.svg.png"/>
+	<img
+		style="height: {Math.max(
+			minHeight,
+			Math.min(maxHeight, maxHeight - scroll + offset)
+		)}px;"
+		alt=""
+		src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/498px-Svelte_Logo.svg.png"
+	/>
 </div>
-
-
 
 <h1>Hello {name}!</h1>
 
 {#each [...Array(50).keys()] as k}
-<p>make page longer</p>
-
+	<p>make page longer</p>
 {/each}
 
 <style>
-
-	#wrapper{
-			position: absolute;
-
-			
+	#wrapper {
+		position: absolute;
 	}
-	
+
 	img {
-		
 		position: sticky;
 		top: 1rem;
-		margin-top:10rem;
+		margin-top: 10rem;
 	}
-	
 </style>
