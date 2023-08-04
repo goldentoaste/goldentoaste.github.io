ed66ac62323e71b2eb74abf8f25a3ae371c545cf
goldentoaste <rayg2375@gmail.com>
2023-08-03 17:41:36 -0700

    fixed resume redirect in static rendering

--- src/routes/(main)/resume/+page.js
+++
@@ -1,6 +0,0 @@
-import { redirect } from '@sveltejs/kit';
- 
-export function load() {
-    window.open('/Resume_RayGong.pdf', '_blank')?.focus()
-    // throw redirect(301, '/Resume_RayGong.pdf');
-}
